5699d99094af9ddf1e0abac0869d91a63374b4d7
neritinc <szu.pet99@gmail.com>
2026-03-05 20:44:18 +0100

    asd

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: "/", name: "home", component: () => import("@/views/HomeView.vue"), meta: { title: "Home", breadcrumb: "Home" } },
-  { path: "/login", name: "login", component: () => import("@/views/LoginView.vue"), meta: { title: "Login", breadcrumb: "Login" } },
-  { path: "/registration", name: "registration", component: () => import("@/views/RegistrationView.vue"), meta: { title: "Registration", breadcrumb: "Registration" } },
-  { path: "/about", name: "about", component: () => import("@/views/AboutView.vue"), meta: { title: "About", breadcrumb: "About" } },
-  { path: "/tracks", name: "tracks", component: () => import("@/views/TracksView.vue"), meta: { title: "Tracks", breadcrumb: "Tracks" } },
-  { path: "/tracks/:id", name: "track-detail", component: () => import("@/views/TrackDetailView.vue"), meta: { title: "Track Details", breadcrumb: "Track Details" } },
-  { path: "/genres", name: "genres", component: () => import("@/views/GenresView.vue"), meta: { title: "Genres", breadcrumb: "Genres", roles: [1] } },
-  { path: "/artists", name: "artists", component: () => import("@/views/ArtistsView.vue"), meta: { title: "Artists", breadcrumb: "Artists", roles: [1] } },
-  { path: "/my-cart", name: "my-cart", component: () => import("@/views/MyCartView.vue"), meta: { title: "My Cart", breadcrumb: "My Cart", roles: [1, 2] } },
-  { path: "/admin-carts", name: "admin-carts", component: () => import("@/views/AdminCartsView.vue"), meta: { title: "All Carts", breadcrumb: "All Carts", roles: [1] } },
-  { path: "/:pathMatch(.*)*", name: "not-found", component: () => import("@/views/404.vue"), meta: { title: "404", breadcrumb: "404" } },
+  { path: "/", name: "home", component: () => import("../views/HomeView.vue"), meta: { title: "Home", breadcrumb: "Home" } },
+  { path: "/login", name: "login", component: () => import("../views/LoginView.vue"), meta: { title: "Login", breadcrumb: "Login" } },
+  { path: "/registration", name: "registration", component: () => import("../views/RegistrationView.vue"), meta: { title: "Registration", breadcrumb: "Registration" } },
+  { path: "/about", name: "about", component: () => import("../views/AboutView.vue"), meta: { title: "About", breadcrumb: "About" } },
+  { path: "/tracks", name: "tracks", component: () => import("../views/TracksView.vue"), meta: { title: "Tracks", breadcrumb: "Tracks" } },
+  { path: "/tracks/:id", name: "track-detail", component: () => import("../views/TrackDetailView.vue"), meta: { title: "Track Details", breadcrumb: "Track Details" } },
+  { path: "/genres", name: "genres", component: () => import("../views/GenresView.vue"), meta: { title: "Genres", breadcrumb: "Genres", roles: [1] } },
+  { path: "/artists", name: "artists", component: () => import("../views/ArtistsView.vue"), meta: { title: "Artists", breadcrumb: "Artists", roles: [1] } },
+  { path: "/my-cart", name: "my-cart", component: () => import("../views/MyCartView.vue"), meta: { title: "My Cart", breadcrumb: "My Cart", roles: [1, 2] } },
+  { path: "/admin-carts", name: "admin-carts", component: () => import("../views/AdminCartsView.vue"), meta: { title: "All Carts", breadcrumb: "All Carts", roles: [1] } },
+  { path: "/:pathMatch(.*)*", name: "not-found", component: () => import("../views/404.vue"), meta: { title: "404", breadcrumb: "404" } },
 ];
 
 const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes });
